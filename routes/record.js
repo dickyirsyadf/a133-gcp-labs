f721263fbfde4dd2f26e1a22b1b6ec9375430268
Dicky Irsyad <dickyirsyadf@gmail.com>
2023-11-06 19:32:39 +0000

    finishing TODO

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '34.143.142.74',
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: 'dicoding',
+    password: 'dickyirsyad02'
 })
 
 router.get("/dashboard", (req, res) => {
